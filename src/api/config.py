5acd4b74a92a63b21c42e76772bf802aff12397a
JeanExtreme002 <jeangamerextreme@gmail.com>
2024-04-08 23:28:56 -0300

    fix port

--- src/api/config.py
+++ src/api/config.py
@@ -23,7 +23,7 @@ class DBConfig:
     DB_USERNAME = os.getenv("DB_USERNAME", ...)
     DB_PASSWORD = os.getenv("DB_PASSWORD", ...)
     DB_HOST = os.getenv("DB_HOST", ...)
-    DB_PORT = os.getenv("DB_PORT", ...)
+    DB_PORT: int = int(os.getenv("DB_PORT", ...))
     DB_DATABASE = os.getenv("DB_DATABASE", ...)
 
 
