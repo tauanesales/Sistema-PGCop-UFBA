31c566f7885243a4d5ec985b988479f439bbbf0c
JeanExtreme002 <jeangamerextreme@gmail.com>
2024-04-09 02:37:49 -0300

    improved test_login

--- src/api/tests/test_login_route.py
+++ src/api/tests/test_login_route.py
@@ -21,4 +21,28 @@ def test_login():
     response = client.post("token/", data=login_data, headers={"content-type": "application/x-www-form-urlencoded"})
     assert 200 <= response.status_code <= 299
 
+    # Try to invade the account.
+    test_cases = [
+        {"username": email, "grant_type": "password"},
+        {"username": email, "password": "", "grant_type": "password"},
+        {"username": email, "password": " ", "grant_type": "password"},
+        {"username": email, "password": " " * len(password), "grant_type": "password"},
+        {"username": email, "password": password[:-1], "grant_type": "password"},
+        {"username": email, "password": password.capitalize(), "grant_type": "password"},
+        {"username": email, "password": password.upper(), "grant_type": "password"},
+    ]
+
+    for test_case in test_cases:
+        response = client.post("token/", data=test_case, headers={"content-type": "application/x-www-form-urlencoded"})
+        assert response.status_code >= 400
+
+    # Try to log in to an account of different username but same password.
+    login_data = {
+        "username": email.split("@")[0] + "@ufmg.br", 
+        "password": password, 
+        "grant_type": "password"
+    }
+
+    response = client.post("token/", data=login_data, headers={"content-type": "application/x-www-form-urlencoded"})
+    assert response.status_code >= 400
 
