761f301cb477a857192e5b1dca51e6913e69eb4c
Pedro Harzer <pedroharzer@gmail.com>
2024-04-14 14:52:52 -0300

    [ADD] Added aluno model and route

--- src/api/entrypoints/router.py
+++ src/api/entrypoints/router.py
@@ -3,8 +3,10 @@ from fastapi.routing import APIRouter
 from src.api.entrypoints import monitoring
 from src.api.entrypoints import usuario
 from src.api.entrypoints import token
+from src.api.entrypoints import aluno
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(usuario.router, prefix="/usuarios", tags=["Usuários"])
+api_router.include_router(aluno.router, prefix="/alunos", tags=["Alunos"])
 api_router.include_router(token.router, prefix="/token", tags=["Token"])
