f1603080f2f4f1029a93db3c69b9534fa92ce95e
JeanExtreme002 <jeangamerextreme@gmail.com>
2024-04-15 13:30:03 -0300

    fixed views for alunos

--- src/api/entrypoints/alunos/views.py
+++ src/api/entrypoints/alunos/views.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Session
 
 
 from src.api.database.session import get_db
-from src.api.entrypoints.alunos.errors import CPFAlreadyRegisteredException, StudentNotFoundException
 from src.api.entrypoints.alunos.schema import AlunoBase, AlunoInDB
 from src.api.services.aluno import ServiceAluno
 
@@ -12,42 +11,25 @@ router = APIRouter()
 
 @router.post("/alunos", response_model=AlunoInDB, status_code=status.HTTP_201_CREATED)
 def criar_aluno(aluno: AlunoBase, db: Session = Depends(get_db)):
-    db_aluno = ServiceAluno.obter_aluno_por_cpf(db, cpf=aluno.Cpf)
-    if db_aluno:
-        raise CPFAlreadyRegisteredException()
     return ServiceAluno.criar_aluno(db, aluno)
 
 @router.get("/alunos/{aluno_id}", response_model=AlunoInDB)
 def get_aluno(aluno_id: int, db: Session = Depends(get_db)):
-    db_aluno = ServiceAluno.obter_aluno(db, aluno_id)
-    if db_aluno is None:
-        raise StudentNotFoundException()
-    return db_aluno
+    return ServiceAluno.obter_aluno(db, aluno_id)
 
 @router.delete("/alunos/{aluno_id}", status_code=status.HTTP_204_NO_CONTENT)
 def deletar_aluno(aluno_id: int, db: Session = Depends(get_db)):
-    req = ServiceAluno.deletar_aluno(db, aluno_id)
-    if not req:
-        raise StudentNotFoundException()
+    ServiceAluno.deletar_aluno(db, aluno_id)
     return {"ok": True}
 
 @router.put("/alunos/{aluno_id}", response_model=AlunoInDB)
 def atualizar_aluno(aluno_id: int, aluno: AlunoBase, db: Session = Depends(get_db)):
-    db_aluno = ServiceAluno.atualizar_aluno(db, aluno_id, aluno.dict())
-    if db_aluno is None:
-        raise StudentNotFoundException()
-    return db_aluno
+    return ServiceAluno.atualizar_aluno(db, aluno_id, aluno.dict())
 
 @router.get("/alunos/cpf/{aluno_cpf}", response_model=AlunoInDB)
 def get_aluno_cpf(aluno_cpf: str, db: Session = Depends(get_db)):
-    db_aluno = ServiceAluno.obter_aluno_por_cpf(db, aluno_cpf)
-    if db_aluno is None:
-        raise StudentNotFoundException()
-    return db_aluno
+    return ServiceAluno.obter_aluno_por_cpf(db, aluno_cpf)
 
 @router.get("/alunos/email/{aluno_email}", response_model=AlunoInDB)
 def get_aluno_email(aluno_email: str, db: Session = Depends(get_db)):
-    db_aluno = ServiceAluno.obter_aluno_por_email(db, aluno_email)
-    if db_aluno is None:
-        raise StudentNotFoundException()
-    return db_aluno
+    return ServiceAluno.obter_aluno_por_email(db, aluno_email)
--- src/api/services/aluno.py
+++ src/api/services/aluno.py
@@ -1,12 +1,21 @@
 from sqlalchemy.orm import Session
 
 from src.api.database.models.aluno import Aluno
+from src.api.entrypoints.alunos.errors import CPFAlreadyRegisteredException, StudentNotFoundException
 from src.api.entrypoints.alunos.schema import AlunoBase
 
 
 class ServiceAluno:
     @staticmethod
     def criar_aluno(db: Session, aluno: AlunoBase):
+
+        # CPF must not be registered.
+        try: 
+            ServiceAluno.obter_aluno_por_cpf(db, cpf=aluno.Cpf):
+            raise CPFAlreadyRegisteredException()
+        except StudentNotFoundException:
+            pass
+
         db_aluno = Aluno(
             Nome=aluno.Nome,
             Cpf=aluno.Cpf,
@@ -19,11 +28,17 @@ class ServiceAluno:
         db.add(db_aluno)
         db.commit()
         db.refresh(db_aluno)
+
         return db_aluno
 
     @staticmethod
     def obter_aluno(db: Session, aluno_id: int):
-        return db.query(Aluno).filter(Aluno.UserID == aluno_id).one_or_none()
+        db_aluno = db.query(Aluno).filter(Aluno.UserID == aluno_id).one_or_none()
+
+        if db_aluno is None:
+            raise StudentNotFoundException()
+
+        return db_aluno
 
     @staticmethod
     def deletar_aluno(db: Session, aluno_id: int):
@@ -32,20 +47,35 @@ class ServiceAluno:
         if db_aluno:
             db.delete(db_aluno)
             db.commit()
-            return True
 
-        return False
+        raise StudentNotFoundException()
 
     @staticmethod
     def atualizar_aluno(db: Session, aluno_id: int, update_data: dict):
         db.query(Aluno).filter(Aluno.UserID == aluno_id).update(update_data)
         db.commit()
-        return db.query(Aluno).filter(Aluno.UserID == aluno_id).one()
+
+        db_aluno = db.query(Aluno).filter(Aluno.UserID == aluno_id).one()
+        
+        if db_aluno is None:
+            raise StudentNotFoundException()
+
+        return db_aluno
 
     @staticmethod
     def obter_aluno_por_email(db: Session, email: str):
-        return db.query(Aluno).filter(Aluno.Email == email).one_or_none()
+        db_aluno = db.query(Aluno).filter(Aluno.Email == email).one_or_none()
+
+        if db_aluno is None:
+            raise StudentNotFoundException()
+
+        return db_aluno
 
     @staticmethod
     def obter_aluno_por_cpf(db: Session, cpf: str):
-        return db.query(Aluno).filter(Aluno.Cpf == cpf).one_or_none()
+        db_aluno = db.query(Aluno).filter(Aluno.Cpf == cpf).one_or_none()
+
+        if db_aluno is None:
+            raise StudentNotFoundException()
+
+        return db_aluno
