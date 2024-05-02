97f364d79319fb1bb1bdfc9b52df35dd4bd288b9
JeanExtreme002 <jeangamerextreme@gmail.com>
2024-05-02 15:39:23 -0300

    update Aluno model

--- src/api/database/models/aluno.py
+++ src/api/database/models/aluno.py
@@ -1,15 +1,18 @@
-from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy import Column, Date, Enum, Integer, String
 from src.api.database.session import Base
 
 
 class Aluno(Base):
-    __tablename__ = "ALUNOS"
-    AlunoID = Column(Integer, primary_key=True, index=True)
-    Nome = Column(String, nullable=False, unique=False, index=True)
-    Cpf = Column(String, nullable=False, unique=True, index=True)
-    Email = Column(String, unique=True, index=True, nullable=False)
-    Telefone = Column(String, nullable=False, unique=True, index=True)
-    Matricula = Column(String, nullable=False, unique=True, index=True)
-    ProfessorID = Column(Integer, nullable=False, index=True)
-    Role = Column(Enum("mestrando", "doutorando"), nullable=False)
-    DataDeIngresso = Column(Date, nullable=False)
+    __tablename__ = "alunos"
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False, unique=False, index=False)
+    cpf = Column(String, nullable=False, unique=True, index=True)
+    email_ufba = Column(String, unique=True, index=True, nullable=False)
+    matricula = Column(String, nullable=False, unique=True, index=True)
+    lattes = Column(String, nullable=True, index=False)
+    orientador_id = Column(Integer, nullable=True, index=True)
+    curso = Column(Enum("M", "D"), nullable=False)
+    data_ingresso = Column(Date, nullable=True)
+    data_qualificacao = Column(Date, nullable=False)
+    data_defesa = Column(Date, nullable=False)
+    senha_hash = Column(String, nullable=False)
