09a116cd68a73f2ce227d60d685c0b589e0df132
Tauane Sales <taysales6@gmail.com>
2024-05-19 18:10:07 -0300

    feat: add token expiration date to /token route

--- src/api/entrypoints/token/schema.py
+++ src/api/entrypoints/token/schema.py
@@ -1,46 +1,33 @@
 from pydantic import BaseModel, EmailStr, constr, validator
-from typing import Optional, Literal
+from typing import Optional
 from enum import Enum
+from datetime import datetime
+
 
 class TokenType(Enum):
     ACCESS = "access"
     REFRESH = "refresh"
 
+
 class Token(BaseModel):
     access_token: str
     token_type: str
+    expiration_date: datetime
+
 
 class TokenData(BaseModel):
     username: Optional[str] = None
     exp: Optional[int] = None  # Expiry timestamp
 
+
 class Login(BaseModel):
     email: EmailStr
     senha: constr(min_length=8)  # Exigindo mínimo de 8 caracteres
 
-    @validator('senha')
+    @validator("senha")
     def senha_strength(cls, value):
         if not any(char.isdigit() for char in value):
             raise ValueError("Senha deve conter pelo menos um número")
         if not any(char.isupper() for char in value):
             raise ValueError("Senha deve conter pelo menos uma letra maiúscula")
         return value
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
