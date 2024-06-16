33a293ef869568e026fb8a5b8639e40704e5f549
Tauane Sales <taysales6@gmail.com>
2024-06-16 01:55:08 -0300

    fix: add table relationships

--- src/api/database/models/tarefa.py
+++ src/api/database/models/tarefa.py
@@ -1,13 +1,17 @@
-from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy import Column, Date, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
 from src.api.database.session import Base
+
 
 class Tarefa(Base):
     __tablename__ = "tarefas"
     id = Column(Integer, primary_key=True, index=False)
     nome = Column(String(255), nullable=False)
     descricao = Column(String(255), nullable=False)
-    completada = Column(Integer,nullable=False, default=0)
+    completada = Column(Integer, nullable=False, default=0)
     data_prazo = Column(Date, nullable=False)
-    aluno_id = Column(Integer, nullable=False)
+    aluno_id = Column(Integer, ForeignKey("alunos.id"), nullable=False)
+    aluno = relationship("Aluno", back_populates="tarefas")
     last_notified = Column(Date, nullable=True)
     data_conclusao = Column(Date, nullable=True)
