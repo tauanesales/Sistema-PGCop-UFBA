ba87b1488a7a6e93c8ca698c7b35bdb8cf83ad31
Tauane Sales <taysales6@gmail.com>
2024-06-23 17:51:48 -0300

    chore: renomeando variaveis/refactor/code formatter

--- src/api/entrypoints/solicitacao/views.py
+++ src/api/entrypoints/solicitacao/views.py
@@ -1,6 +1,6 @@
-from loguru import logger
 from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordBearer
+from loguru import logger
 
 from src.api.database.models.professor import Professor
 from src.api.database.session import get_repo
@@ -8,9 +8,7 @@ from src.api.entrypoints.solicitacao.schema import SolicitacaoInDB
 from src.api.exceptions.credentials_exception import NaoAutorizadoException
 from src.api.services.solicitacao import ServicoSolicitacao
 from src.api.services.tipo_usuario import ServicoTipoUsuario
-from src.api.utils.enums import StatusSolicitacaoEnum
-from src.api.utils.enums import TipoUsuarioEnum
-
+from src.api.utils.enums import StatusSolicitacaoEnum, TipoUsuarioEnum
 
 router = APIRouter()
 
@@ -22,38 +20,57 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
     response_model=list[SolicitacaoInDB],
     status_code=status.HTTP_200_OK,
 )
-async def listar_solicitacoes(professor_id: int, status: StatusSolicitacaoEnum, token: str = Depends(oauth2_scheme), repository=Depends(get_repo())):  
-    logger.info(f"Solicitada listagem de solicitações pelo professor com id {professor_id} | Autenticando usuário atual.")
-    professor: Professor = await ServicoTipoUsuario(repository).buscar_usuario_atual(token=token)
+async def listar_solicitacoes(
+    professor_id: int,
+    status: StatusSolicitacaoEnum,
+    token: str = Depends(oauth2_scheme),
+    repository=Depends(get_repo()),
+):
+    logger.info(
+        f"Solicitada listagem de solicitações pelo professor com id {professor_id}"
+        f" | Autenticando usuário atual."
+    )
+    professor: Professor = await ServicoTipoUsuario(repository).buscar_usuario_atual(
+        token=token
+    )
     logger.info(
         f"{professor_id=} {professor.id=} | "
         f"Tipo usuário atual é {professor.usuario.tipo_usuario.titulo}."
     )
-    if professor.usuario.tipo_usuario.titulo not in [TipoUsuarioEnum.COORDENADOR, TipoUsuarioEnum.PROFESSOR]:
+    if professor.usuario.tipo_usuario.titulo not in [
+        TipoUsuarioEnum.COORDENADOR,
+        TipoUsuarioEnum.PROFESSOR,
+    ]:
         raise NaoAutorizadoException()
     return await ServicoSolicitacao(repository).listar(
         professor_id=professor_id, status=status
     )
 
 
-
-
 @router.put(
     "/{solicitacao_id}", response_model=SolicitacaoInDB, status_code=status.HTTP_200_OK
 )
 async def atualizar_status_solicitacao(
-    solicitacao_id: int, 
-    status: StatusSolicitacaoEnum, 
+    solicitacao_id: int,
+    status: StatusSolicitacaoEnum,
     repository=Depends(get_repo()),
-    token: str = Depends(oauth2_scheme)
+    token: str = Depends(oauth2_scheme),
 ):
-    logger.info(f"Atualização da solicitação de id {solicitacao_id} | Autenticando usuário atual.")
-    coordenador: Professor = await ServicoTipoUsuario(repository).buscar_usuario_atual(token=token)
     logger.info(
-        f"{solicitacao_id=} {coordenador.id=} | "
-        f"Tipo usuário atual é {coordenador.usuario.tipo_usuario.titulo}."
+        f"Atualização da solicitação de id {solicitacao_id}"
+        f" | Autenticando usuário atual."
     )
-    if (coordenador.usuario.tipo_usuario.titulo != TipoUsuarioEnum.COORDENADOR) and (coordenador.usuario.tipo_usuario.titulo != TipoUsuarioEnum.PROFESSOR):
+    professor: Professor = await ServicoTipoUsuario(repository).buscar_usuario_atual(
+        token=token
+    )
+    logger.info(
+        f"{solicitacao_id=} {professor.id=} | "
+        f"Tipo usuário atual é {professor.usuario.tipo_usuario.titulo}."
+    )
+    if professor.usuario.tipo_usuario.titulo not in [
+        TipoUsuarioEnum.COORDENADOR,
+        TipoUsuarioEnum.PROFESSOR,
+    ]:
         raise NaoAutorizadoException()
     return await ServicoSolicitacao(repository).atualizar_status_solicitacao(
         solicitacao_id=solicitacao_id, status=status
