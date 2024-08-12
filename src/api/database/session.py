746974257574b4e4d2a5612b9952821eda610450
Tauane Sales <taysales6@gmail.com>
2024-08-12 18:10:23 -0300

    fix: change db to postgres

--- src/api/database/session.py
+++ src/api/database/session.py
@@ -17,7 +17,8 @@ engine = create_async_engine(
         host=Config.DB_CONFIG.DB_HOST,
         port=Config.DB_CONFIG.DB_PORT,
         database=Config.DB_CONFIG.DB_DATABASE,
-        query={"charset": "UTF8MB4"},
+        # query={"charset": "UTF8MB4"},
+        query={},
     ),
     poolclass=AsyncAdaptedQueuePool
     if Config.DB_CONFIG.DB_ENABLE_CONNECTION_POOLING
